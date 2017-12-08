70874962b61df92862b6cb617c9483f1edaf6c49
amecea <flavius.mecea@gmail.com>
2017-12-08 12:03:33 +0200

    Add google credentials for backup secret.

--- tests/conftest.py
+++ tests/conftest.py
@@ -76,8 +76,7 @@ class Release:
 
             out = sh.helm.install('-f', config_file.name, '--name', self.release, '--debug',
                                   '--namespace', self.namespace, CHART_PATH)
-            print (out)
-            #print('\n'.join(out.stdout.decode('utf-8').splitlines()[-25:]))
+            print('\n'.join(out.stdout.decode('utf-8').splitlines()[-25:]))
 
     def delete(self):
         sh.helm.delete('--purge', self.release, '--timeout', 10, '--no-hooks')
