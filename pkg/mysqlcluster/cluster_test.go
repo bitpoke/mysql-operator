5fa15f4de111d237cf6381ecdeff4a68e8537752
amecea <flavius.mecea@gmail.com>
2018-03-28 17:03:08 +0300

    Add licence to every file.

--- pkg/mysqlcluster/cluster_test.go
+++ pkg/mysqlcluster/cluster_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Pressinfra SRL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package mysqlcluster
 
 import (
@@ -180,4 +196,9 @@ func TestSyncClusterCreationWithSecret(t *testing.T) {
 		return
 	}
 
+	_, err = client.BatchV1().CronJobs(ns).Get(cluster.GetNameForResource(api.StatefulSet), metav1.GetOptions{})
+	if err != nil {
+		t.Fail()
+		return
+	}
 }
