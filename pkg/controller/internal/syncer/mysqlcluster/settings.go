1cf6316ad52dc562fa2ab7af6e3b325fccafc9e4
amecea <flavius@presslabs.com>
2018-09-12 12:58:24 +0300

    Create a util/constants package with constants.

--- pkg/controller/internal/syncer/mysqlcluster/settings.go
+++ pkg/controller/internal/syncer/mysqlcluster/settings.go
@@ -17,47 +17,42 @@ limitations under the License.
 package mysqlcluster
 
 import (
+	"github.com/presslabs/mysql-operator/pkg/util/constants"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
+// TODO: make those consts private and move them in the file where are used.
 const (
 	// MysqlPortName represents the mysql port name.
 	MysqlPortName = "mysql"
 	// MysqlPort is the default mysql port.
-	MysqlPort = 3306
+	MysqlPort = constants.MysqlPort
 
 	// HelperXtrabackupPortName is name of the port on which we take backups
 	HelperXtrabackupPortName = "xtrabackup"
 	// HelperXtrabackupPort is the port on which we serve backups
-	HelperXtrabackupPort = 3307
+	HelperXtrabackupPort = constants.HelperXtrabackupPort
 
 	// OrcTopologyDir path where orc conf secret is mounted
-	OrcTopologyDir = "/var/run/orc-topology"
-
-	// ConfigVersion is the mysql config that needs to be updated if configs
-	// change
-	ConfigVersion = "2018-03-23:12:33"
+	OrcTopologyDir = constants.OrcTopologyDir
 
 	// HelperServerPort represents the port on which http server will run
-	HelperServerPort = 8088
+	HelperServerPort = constants.HelperServerPort
 	// HelperServerProbePath the probe path
-	HelperServerProbePath = "/health"
+	HelperServerProbePath = constants.HelperServerProbePath
 
 	// ExporterPort is the port that metrics will be exported
-	ExporterPort = 9125
+	ExporterPort = constants.ExporterPort
 
 	//ExporterPortName the name of the metrics exporter port
 	ExporterPortName = "prometheus"
 
 	// ExporterPath is the path on which metrics are expose
-	ExporterPath = "/metrics"
+	ExporterPath = constants.ExporterPath
 
 	// HelperDbName represent the database name that is used by operator to
-	// manage the mysql cluster. This database contains a table with
-	// initialization history and table managed by pt-heartbeat. Be aware that
-	// when changeing this value to update the orchestrator chart value for
-	// SlaveLagQuery in hack/charts/mysql-operator/values.yaml.
-	HelperDbName = "sys_operator"
+	// manage the mysql cluster.
+	HelperDbName = constants.HelperDbName
 )
 
 var (
@@ -66,63 +61,4 @@ var (
 
 	// ExporterTargetPort is the port for the exporter
 	ExporterTargetPort = intstr.FromInt(ExporterPort)
-
-	// MysqlMasterSlaveConfigs represents the configuration that mysql-operator needs by default
-	MysqlMasterSlaveConfigs = map[string]string{
-		"log-bin":           "/var/lib/mysql/mysql-bin",
-		"log-slave-updates": "on",
-
-		"read-only":        "on",
-		"skip-slave-start": "on",
-
-		// Crash safe
-		"relay-log-info-repository": "TABLE",
-		"relay-log-recovery":        "on",
-
-		// https://github.com/github/orchestrator/issues/323#issuecomment-338451838
-		"master_info_repository": "TABLE",
-
-		"default-storage-engine":   "InnoDB",
-		"gtid-mode":                "on",
-		"enforce-gtid-consistency": "on",
-
-		// MyISAM
-		"key-buffer-size":        "32M",
-		"myisam-recover-options": "FORCE,BACKUP",
-
-		// Safety
-		"max-allowed-packet": "16M",
-		"max-connect-errors": "1000000",
-		"sql-mode": "STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER," +
-			"NO_AUTO_VALUE_ON_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY",
-		"sysdate-is-now": "1",
-
-		// Binary logging
-		"expire-logs-days": "14",
-		"sync-binlog":      "1",
-		"binlog-format":    "ROW",
-
-		// CACHES AND LIMITS
-		"tmp-table-size":         "32M",
-		"max-heap-table-size":    "32M",
-		"query-cache-type":       "0",
-		"query-cache-size":       "0",
-		"max-connections":        "500",
-		"thread-cache-size":      "50",
-		"open-files-limit":       "65535",
-		"table-definition-cache": "4096",
-		"table-open-cache":       "4096",
-
-		// InnoDB
-		"innodb-flush-method":            "O_DIRECT",
-		"innodb-log-files-in-group":      "2",
-		"innodb-flush-log-at-trx-commit": "2",
-		"innodb-file-per-table":          "1",
-
-		"character-set-server": "utf8mb4",
-		"collation-server":     "utf8mb4_unicode_ci",
-
-		"skip-name-resolve": "on",
-		"skip-host-cache":   "on",
-	}
 )
---
+++ pkg/util/constants/constants.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2018 Pressinfra SRL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package constants
+
+const (
+	// MysqlPort is the default mysql port.
+	MysqlPort = 3306
+
+	// HelperXtrabackupPort is the port on which we serve backups
+	HelperXtrabackupPort = 3307
+
+	// OrcTopologyDir path where orc conf secret is mounted
+	OrcTopologyDir = "/var/run/orc-topology"
+
+	// HelperServerPort represents the port on which http server will run
+	HelperServerPort = 8088
+	// HelperServerProbePath the probe path
+	HelperServerProbePath = "/health"
+
+	// ExporterPort is the port that metrics will be exported
+	ExporterPort = 9125
+
+	// ExporterPath is the path on which metrics are expose
+	ExporterPath = "/metrics"
+
+	// HelperDbName represent the database name that is used by operator to
+	// manage the mysql cluster. This database contains a table with
+	// initialization history and table managed by pt-heartbeat. Be aware that
+	// when changeing this value to update the orchestrator chart value for
+	// SlaveLagQuery in hack/charts/mysql-operator/values.yaml.
+	HelperDbName = "sys_operator"
+)
