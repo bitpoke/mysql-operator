cf27b98341b096670df45b06753da4ab0e45f6e8
Robert Stefan <rstefan1@users.noreply.github.com>
2019-03-25 13:22:49 +0200

    Remove code duplication

--- pkg/controller/mysqlbackupcron/mysqlbackupcron_controller_suite_test.go
+++ pkg/controller/mysqlbackupcron/mysqlbackupcron_controller_suite_test.go
@@ -26,8 +26,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
 	"github.com/presslabs/mysql-operator/pkg/apis"
@@ -60,25 +58,3 @@ var _ = BeforeSuite(func() {
 var _ = AfterSuite(func() {
 	t.Stop()
 })
-
-// SetupTestReconcile returns a reconcile.Reconcile implementation that delegates to inner and
-// writes the request to requests after Reconcile is finished.
-func SetupTestReconcile(inner reconcile.Reconciler) (reconcile.Reconciler, chan reconcile.Request) {
-	requests := make(chan reconcile.Request)
-	fn := reconcile.Func(func(req reconcile.Request) (reconcile.Result, error) {
-		result, err := inner.Reconcile(req)
-		requests <- req
-		return result, err
-	})
-	return fn, requests
-}
-
-// StartTestManager adds recFn
-func StartTestManager(mgr manager.Manager) chan struct{} {
-	stop := make(chan struct{})
-	go func() {
-		defer GinkgoRecover()
-		Expect(mgr.Start(stop)).NotTo(HaveOccurred())
-	}()
-	return stop
-}
