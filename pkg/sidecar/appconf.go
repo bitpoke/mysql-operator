beb41ce88d87effdb2086a3d5a6bea821e791013
amecea <flavius@presslabs.com>
2019-04-03 21:04:04 +0300

    Set credentials with the init-file

--- pkg/sidecar/appconf.go
+++ pkg/sidecar/appconf.go
@@ -18,20 +18,17 @@ package sidecar
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"strconv"
+	"strings"
 
 	"github.com/go-ini/ini"
-
-	pkgutil "github.com/presslabs/mysql-operator/pkg/util"
-)
-
-const (
-	rStrLen = 18
 )
 
 // RunConfigCommand generates my.cnf, client.cnf and 10-dynamic.cnf files.
+// nolint: gocyclo
 func RunConfigCommand(cfg *Config) error {
 	log.Info("configuring server", "host", cfg.Hostname, "role", cfg.NodeRole())
 	var err error
@@ -46,10 +43,9 @@ func RunConfigCommand(cfg *Config) error {
 		}
 	}
 
-	uPass := pkgutil.RandomString(rStrLen)
 	reportHost := cfg.FQDNForServer(cfg.ServerID())
 
-	var identityCFG, utilityCFG, clientCFG *ini.File
+	var identityCFG, initCFG, clientCFG *ini.File
 
 	// mysql server identity configs
 	if identityCFG, err = getIdentityConfigs(cfg.ServerID(), reportHost); err != nil {
@@ -59,16 +55,21 @@ func RunConfigCommand(cfg *Config) error {
 		return fmt.Errorf("failed to save configs: %s", err)
 	}
 
-	// mysql server utility user configs
-	if utilityCFG, err = getUtilityUserConfigs(utilityUser, uPass); err != nil {
-		return fmt.Errorf("failed to configure utility user: %s", err)
+	// make init-file
+	initFilePath := path.Join(confDPath, "operator-init.sql")
+	if err = ioutil.WriteFile(initFilePath, initFileQuery(cfg), 0644); err != nil {
+		return fmt.Errorf("failed to write init-file: %s", err)
 	}
-	if err = utilityCFG.SaveTo(path.Join(confDPath, "10-utility-user.cnf")); err != nil {
-		return fmt.Errorf("failed to configure utility user: %s", err)
+	// mysql server utility user configs
+	if initCFG, err = getInitFileConfigs(initFilePath); err != nil {
+		return fmt.Errorf("failed to configure init file: %s", err)
+	}
+	if err = initCFG.SaveTo(path.Join(confDPath, "10-init-file.cnf")); err != nil {
+		return fmt.Errorf("failed to configure init file: %s", err)
 	}
 
 	// mysql client connect credentials
-	if clientCFG, err = getClientConfigs(utilityUser, uPass); err != nil {
+	if clientCFG, err = getClientConfigs(cfg.OperatorUser, cfg.OperatorPassword); err != nil {
 		return fmt.Errorf("failed to get client configs: %s", err)
 	}
 
@@ -114,25 +115,58 @@ func getIdentityConfigs(id int, reportHost string) (*ini.File, error) {
 	return cfg, nil
 }
 
-func getUtilityUserConfigs(user, pass string) (*ini.File, error) {
+func getInitFileConfigs(filePath string) (*ini.File, error) {
 	cfg := ini.Empty()
 	mysqld := cfg.Section("mysqld")
 
-	if _, err := mysqld.NewKey("utility-user", fmt.Sprintf("%s@%%", user)); err != nil {
-		return nil, err
-	}
-	if _, err := mysqld.NewKey("utility-user-password", pass); err != nil {
-		return nil, err
-	}
-	if _, err := mysqld.NewKey("utility-user-schema-access", "mysql"); err != nil {
-		return nil, err
-	}
-	if _, err := mysqld.NewKey("utility-user-privileges",
-		"SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,GRANT,ALTER,SHOW DATABASES,SUPER,CREATE USER,"+
-			"PROCESS,RELOAD,LOCK TABLES,REPLICATION CLIENT,REPLICATION SLAVE",
-	); err != nil {
+	if _, err := mysqld.NewKey("init-file", filePath); err != nil {
 		return nil, err
 	}
 
 	return cfg, nil
+}
+
+func initFileQuery(cfg *Config) []byte {
+	queries := []string{
+		"SET @@SESSION.SQL_LOG_BIN = 0;",
+	}
+
+	queries = append(queries, createUserQuery(cfg.OperatorUser, cfg.OperatorPassword, "%", []string{"SUPER"}, "*.*"))
+	queries = append(queries, createUserQuery(cfg.OrchestratorUser, cfg.OrchestratorPassword, "%",
+		[]string{"SUPER", "PROCESS", "REPLICATION SLAVE", "REPLICATION CLIENT", "RELOAD"}, "*.*",
+		[]string{"SELECT"}, "mysql.slave_master_info"))
+	queries = append(queries, createUserQuery(cfg.ReplicationUser, cfg.ReplicationPassword, "%",
+		[]string{"SELECT", "PROCESS", "RELOAD", "LOCK TABLES", "REPLICATION CLIENT", "REPLICATION SLAVE"}, "*.*"))
+	queries = append(queries, createUserQuery(cfg.MetricsUser, cfg.MetricsPassword, "127.0.0.1",
+		[]string{"SELECT", "PROCESS", "REPLICATION CLIENT"}, "*.*"))
+
+	return []byte(strings.Join(queries, "\n"))
+}
+
+func createUserQuery(name, pass, host string, rights ...interface{}) string {
+	user := fmt.Sprintf("%s@'%s'", name, host)
+
+	if len(rights)%2 != 0 {
+		panic("not a good number of parameters")
+	}
+	grants := []string{}
+	for i := 0; i < len(rights); i += 2 {
+		var (
+			right []string
+			on    string
+			ok    bool
+		)
+		if right, ok = rights[i].([]string); !ok {
+			panic("[right] not a good parameter")
+		}
+		if on, ok = rights[i+1].(string); !ok {
+			panic("[on] not a good parameter")
+		}
+		grant := fmt.Sprintf("GRANT %s ON %s TO %s;", strings.Join(right, ", "), on, user)
+		grants = append(grants, grant)
+	}
+
+	return fmt.Sprintf("\nCREATE USER IF NOT EXISTS %s;\n"+
+		"ALTER USER %s IDENTIFIED BY '%s';\n%s",
+		user, user, pass, strings.Join(grants, "\n"))
 }
