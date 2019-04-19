1950812732e2c0e5b43a24dd51640ba3d986b549
amecea <flavius@presslabs.com>
2019-04-19 15:32:56 +0300

    Node controller - for configuring MySQL nodes

--- pkg/sidecar/apphelper.go
+++ pkg/sidecar/apphelper.go
@@ -21,161 +21,87 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"time"
 )
 
-const (
-	// timeOut represents the number of tries to check mysql to be ready.
-	timeOut = 60
-	// connRetry represents the number of tries to connect to master server
-	connRetry = 10
-)
+// const (
+// 	// timeOut represents the number of tries to check mysql to be ready.
+// 	timeOut = 60
+// 	// connRetry represents the number of tries to connect to master server
+// 	connRetry = 10
+// )
 
 // RunSidecarCommand is the main command, and represents the runtime helper that
 // configures the mysql server
 func RunSidecarCommand(cfg *Config, stop <-chan struct{}) error {
-	log.Info("start initialization")
+	// TODO: don't create init table in init-files
+	log.Info("doing bootstrap")
+	if gtid, err := readPurgedGTID(); err == nil {
+		log.Info("RESET MASTER and setting GTID_PURGED", "gtid", gtid)
+		if errQ := runQuery(cfg, "RESET MASTER; SET GLOBAL GTID_PURGED=?", gtid); errQ != nil {
+			return errQ
+		}
 
-	// wait for mysql to be ready
-	if err := waitForMysqlReady(cfg); err != nil {
-		return fmt.Errorf("mysql is not ready, err: %s", err)
+		// remove the file to avoid setting GTID_PURGED next time.
+		if errR := os.Remove(fmt.Sprintf("%s/xtrabackup_binlog_info", dataDir)); errR != nil {
+			return errR
+		}
+	} else {
+		log.V(-1).Info("can't determine what GTID to purge", "error", err)
 	}
 
-	// deactivate super read only
-	log.Info("temporary disable SUPER_READ_ONLY")
-	if err := runQuery(cfg, "SET GLOBAL READ_ONLY = 1; SET GLOBAL SUPER_READ_ONLY = 0;"); err != nil {
-		return fmt.Errorf("failed to configure master node, err: %s", err)
-	}
-
-	// if it's slave set replication source (master host)
-	log.V(1).Info("configure topology")
-	if err := configTopology(cfg); err != nil {
-		return err
-	}
-
-	// mark setup as complete by writing a row in config table
-	log.V(1).Info("flag setup as complet")
-	if err := markConfigurationDone(cfg); err != nil {
-		return err
-	}
-
-	// if it's master node then make it writtable else make it read only
-	log.V(1).Info("configure read only flag")
-	if err := configReadOnly(cfg); err != nil {
-		return err
-	}
-
-	log.V(1).Info("start http server")
+	log.Info("start http server for backups")
 	srv := newServer(cfg, stop)
 	return srv.ListenAndServe()
 }
 
-func waitForMysqlReady(cfg *Config) error {
-	log.V(1).Info("wait for mysql to be ready")
-
-	for i := 0; i < timeOut; i++ {
-		time.Sleep(1 * time.Second)
-		if err := runQuery(cfg, "SELECT 1"); err == nil {
-			break
-		}
-	}
-	if err := runQuery(cfg, "SELECT 1"); err != nil {
-		log.V(1).Info("mysql is not ready", "error", err)
-		return err
-	}
-
-	log.V(1).Info("mysql is ready")
-	return nil
-
-}
-
-func configReadOnly(cfg *Config) error {
-	var query string
-	if cfg.NodeRole() == MasterNode {
-		query = "SET GLOBAL READ_ONLY = 0"
-	} else {
-		query = "SET GLOBAL SUPER_READ_ONLY = 1"
-	}
-	if err := runQuery(cfg, query); err != nil {
-		return fmt.Errorf("failed to set read_only config, err: %s", err)
-	}
-	return nil
-}
-
-func configTopology(cfg *Config) error {
-	if cfg.NodeRole() == SlaveNode {
-		log.Info("setting up as slave")
-		if shouldBootstrapNode() {
-			log.Info("doing bootstrap")
-			if gtid, err := readPurgedGTID(); err == nil {
-				log.Info("RESET MASTER and setting GTID_PURGED", "gtid", gtid)
-				if errQ := runQuery(cfg, "RESET MASTER; SET GLOBAL GTID_PURGED=?", gtid); errQ != nil {
-					return errQ
-				}
-			} else {
-				log.V(-1).Info("can't determine what GTID to purge", "error", err)
-			}
-		}
-
-		// slave node
-		query := `
-		  CHANGE MASTER TO MASTER_AUTO_POSITION=1,
-		    MASTER_HOST=?,
-		    MASTER_USER=?,
-		    MASTER_PASSWORD=?,
-		    MASTER_CONNECT_RETRY=?;
-		`
-		if err := runQuery(cfg, query,
-			cfg.MasterFQDN(), cfg.ReplicationUser, cfg.ReplicationPassword, connRetry,
-		); err != nil {
-			return fmt.Errorf("failed to configure slave node, err: %s", err)
-		}
-
-		query = "START SLAVE;"
-		if err := runQuery(cfg, query); err != nil {
-			log.Info("failed to start slave in the simple mode, trying a second method")
-			// TODO: https://bugs.mysql.com/bug.php?id=83713
-			query2 := `
-			  reset slave;
-			  start slave IO_THREAD;
-			  stop slave IO_THREAD;
-			  reset slave;
-			  start slave;
-			`
-			if err := runQuery(cfg, query2); err != nil {
-				return fmt.Errorf("failed to start slave node, err: %s", err)
-			}
-		}
-	}
-
-	return nil
-}
-
-func markConfigurationDone(cfg *Config) error {
-	query := `
-	  SET @@SESSION.SQL_LOG_BIN = 0;
-	  BEGIN;
-	  CREATE DATABASE IF NOT EXISTS %[1]s;
-	      CREATE TABLE IF NOT EXISTS %[1]s.%[2]s  (
-		name varchar(255) NOT NULL,
-		value varchar(255) NOT NULL,
-		inserted_at datetime NOT NULL
-	      ) ENGINE=csv;
-
-	  INSERT INTO %[1]s.%[2]s VALUES ("init_completed", "?", now());
-	  COMMIT;
-	`
-
-	// insert tables and databases names. It's safe because is not user input.
-	// see: https://github.com/golang/go/issues/18478
-	query = fmt.Sprintf(query, toolsDbName, toolsInitTableName)
-
-	if err := runQuery(cfg, query, cfg.Hostname); err != nil {
-		return fmt.Errorf("failed to mark configuration done, err: %s", err)
-	}
-
-	return nil
-}
+// func configTopology(cfg *Config) error {
+// 	if cfg.NodeRole() == SlaveNode {
+// 		log.Info("setting up as slave")
+// 		if shouldBootstrapNode() {
+// 			log.Info("doing bootstrap")
+// 			if gtid, err := readPurgedGTID(); err == nil {
+// 				log.Info("RESET MASTER and setting GTID_PURGED", "gtid", gtid)
+// 				if errQ := runQuery(cfg, "RESET MASTER; SET GLOBAL GTID_PURGED=?", gtid); errQ != nil {
+// 					return errQ
+// 				}
+// 			} else {
+// 				log.V(-1).Info("can't determine what GTID to purge", "error", err)
+// 			}
+// 		}
+//
+// 		// slave node
+// 		query := `
+// 		  CHANGE MASTER TO MASTER_AUTO_POSITION=1,
+// 		    MASTER_HOST=?,
+// 		    MASTER_USER=?,
+// 		    MASTER_PASSWORD=?,
+// 		    MASTER_CONNECT_RETRY=?;
+// 		`
+// 		if err := runQuery(cfg, query,
+// 			cfg.MasterFQDN(), cfg.ReplicationUser, cfg.ReplicationPassword, connRetry,
+// 		); err != nil {
+// 			return fmt.Errorf("failed to configure slave node, err: %s", err)
+// 		}
+//
+// 		query = "START SLAVE;"
+// 		if err := runQuery(cfg, query); err != nil {
+// 			log.Info("failed to start slave in the simple mode, trying a second method")
+// 			// TODO: https://bugs.mysql.com/bug.php?id=83713
+// 			query2 := `
+// 			  reset slave;
+// 			  start slave IO_THREAD;
+// 			  stop slave IO_THREAD;
+// 			  reset slave;
+// 			  start slave;
+// 			`
+// 			if err := runQuery(cfg, query2); err != nil {
+// 				return fmt.Errorf("failed to start slave node, err: %s", err)
+// 			}
+// 		}
+// 	}
+//
+// 	return nil
+// }
 
 // readPurgedGTID returns the GTID from xtrabackup_binlog_info file
 func readPurgedGTID() (string, error) {
--- pkg/sidecar/apphelper_test.go
+++ pkg/sidecar/apphelper_test.go
@@ -16,32 +16,32 @@ limitations under the License.
 
 package sidecar
 
-import (
-	"strings"
+// import (
+// 	"strings"
+//
+// 	. "github.com/onsi/ginkgo"
+// 	. "github.com/onsi/gomega"
+// )
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-)
-
-var _ = Describe("Test sidecar apphelper", func() {
-	It("should find the single gtid set in backup", func() {
-		var (
-			source string = "mysql-bin.000002        6552870 684ca0cf-495e-11e9-9fe8-0a580af407e9:1-176661\n"
-			result string = "684ca0cf-495e-11e9-9fe8-0a580af407e9:1-176661"
-		)
-		Expect(getGTIDFrom(strings.NewReader(source))).To(Equal(result))
-	})
-	It("should find all gtid sets in backup", func() {
-		var (
-			source string = `mysql-bin.006394      154349713       00003306-1111-0000-0000-000000000001:1-48861335,
-00003306-1111-1111-1111-111111111111:1-11000155952,
-00003306-2222-2222-2222-222222222222:1-8706021957
-`
-			result string = "00003306-1111-0000-0000-000000000001:1-48861335," +
-				"00003306-1111-1111-1111-111111111111:1-11000155952," +
-				"00003306-2222-2222-2222-222222222222:1-8706021957"
-		)
-		Expect(getGTIDFrom(strings.NewReader(source))).To(Equal(result))
-	})
-
-})
+// var _ = Describe("Test sidecar apphelper", func() {
+// 	It("should find the single gtid set in backup", func() {
+// 		var (
+// 			source string = "mysql-bin.000002        6552870 684ca0cf-495e-11e9-9fe8-0a580af407e9:1-176661\n"
+// 			result string = "684ca0cf-495e-11e9-9fe8-0a580af407e9:1-176661"
+// 		)
+// 		Expect(getGTIDFrom(strings.NewReader(source))).To(Equal(result))
+// 	})
+// 	It("should find all gtid sets in backup", func() {
+// 		var (
+// 			source string = `mysql-bin.006394      154349713       00003306-1111-0000-0000-000000000001:1-48861335,
+// 00003306-1111-1111-1111-111111111111:1-11000155952,
+// 00003306-2222-2222-2222-222222222222:1-8706021957
+// `
+// 			result string = "00003306-1111-0000-0000-000000000001:1-48861335," +
+// 				"00003306-1111-1111-1111-111111111111:1-11000155952," +
+// 				"00003306-2222-2222-2222-222222222222:1-8706021957"
+// 		)
+// 		Expect(getGTIDFrom(strings.NewReader(source))).To(Equal(result))
+// 	})
+//
+// })
