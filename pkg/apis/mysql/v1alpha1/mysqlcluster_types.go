b2f2c6fe67de101b79859c25800fc96796ed8759
amecea <flavius@presslabs.com>
2019-07-08 14:34:52 +0300

    Allow user to specify versions map for mysql

--- pkg/apis/mysql/v1alpha1/mysqlcluster_types.go
+++ pkg/apis/mysql/v1alpha1/mysqlcluster_types.go
@@ -43,13 +43,16 @@ type MysqlClusterSpec struct {
 	// +kubebuilder:validation:MaxLength=63
 	SecretName string `json:"secretName"`
 
-	// Represents the percona image tag.
+	// Represents the MySQL version that will be run. The available version can be found here:
+	// https://github.com/presslabs/mysql-operator/blob/0fd4641ce4f756a0aab9d31c8b1f1c44ee10fcb2/pkg/util/constants/constants.go#L87
+	// This field should be set even if the Image is set to let the operator know which mysql version is running.
+	// Based on this version the operator can take decisions which features can be used.
 	// Defaults to 5.7
 	// +optional
 	MysqlVersion string `json:"mysqlVersion,omitempty"`
 
 	// To specify the image that will be used for mysql server container.
-	// If this is specified then the mysqlVersion is ignored.
+	// If this is specified then the mysqlVersion is used as source for MySQL server version.
 	// +optional
 	Image string `json:"image,omitempty"`
 
