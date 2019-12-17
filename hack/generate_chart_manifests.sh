2d8f6232292acf1c03d0f3ddc6b5dc0fab73508b
Vladimir Smagin <21h@blindage.org>
2019-12-18 05:48:38 +0700

    remove hook
    change crds manifests generation, move to crds directory
    
    return k8s label
    
    remove app name label

--- hack/generate_chart_manifests.sh
+++ hack/generate_chart_manifests.sh
@@ -3,22 +3,14 @@
 CHART_PATH=../charts/mysql-operator
 CONFIG_PATH=../config
 
-# templates/crds.yaml update
-awk 'FNR==1 && NR!=1 {print "---"}{print}' ${CONFIG_PATH}/crds/*.yaml > ${CHART_PATH}/templates/_crds.yaml
-yq m -d'*' -i ${CHART_PATH}/templates/_crds.yaml chart-metadata.yaml
-yq w -d'*' -i ${CHART_PATH}/templates/_crds.yaml 'metadata.annotations[helm.sh/hook]' crd-install
-yq d -d'*' -i ${CHART_PATH}/templates/_crds.yaml metadata.creationTimestamp
-yq d -d'*' -i ${CHART_PATH}/templates/_crds.yaml status
-yq d -d'*' -i ${CHART_PATH}/templates/_crds.yaml spec.validation
+# crds/crds.yaml update
+awk 'FNR==1 && NR!=1 {print "---"}{print}' ${CONFIG_PATH}/crds/*.yaml > ${CHART_PATH}/crds/crds.yaml
+yq d -d'*' -i ${CHART_PATH}/crds/crds.yaml metadata.creationTimestamp
+yq d -d'*' -i ${CHART_PATH}/crds/crds.yaml status
+yq d -d'*' -i ${CHART_PATH}/crds/crds.yaml spec.validation
 
 # add shortName to CRD until https://github.com/kubernetes-sigs/kubebuilder/issues/404 is solved
-yq w -d1 -i ${CHART_PATH}/templates/_crds.yaml 'spec.names.shortNames[+]' mysql
-
-
-echo '{{- if (and .Values.installCRDs (not (.Capabilities.APIVersions.Has "mysql.presslabs.org/v1alpha1"))) }}' > ${CHART_PATH}/templates/crds.yaml
-cat ${CHART_PATH}/templates/_crds.yaml >> ${CHART_PATH}/templates/crds.yaml
-echo '{{- end }}' >> ${CHART_PATH}/templates/crds.yaml
-rm ${CHART_PATH}/templates/_crds.yaml
+yq w -d1 -i ${CHART_PATH}/crds/crds.yaml 'spec.names.shortNames[+]' mysql
 
 # templates/rbac.yaml update
 cp ${CONFIG_PATH}/rbac/rbac_role.yaml ${CHART_PATH}/templates/rbac.yaml
