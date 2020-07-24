e67a055f39c19d9d73e79b7bcc9e9f07e8530c60
amecea <flavius@presslabs.com>
2020-07-24 19:54:53 +0300

    Fix e2e tests not used import

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -24,14 +24,12 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/go-logr/logr"
+	"github.com/presslabs/mysql-operator/pkg/apis"
+	orc "github.com/presslabs/mysql-operator/pkg/orchestrator"
 	core "k8s.io/api/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-
-	"github.com/presslabs/mysql-operator/pkg/apis"
-	orc "github.com/presslabs/mysql-operator/pkg/orchestrator"
 )
 
 const (
