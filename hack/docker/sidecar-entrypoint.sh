ac4c6fb418ba7fd6089d82dc896ca23457d9f137
Tony Bogdanov <tonybogdanov@gmail.com>
2020-07-31 21:13:38 +0300

    feat: updated sidecar entrypoint to support gdrive

--- hack/docker/sidecar-entrypoint.sh
+++ hack/docker/sidecar-entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 
+echo "Create Google Drive service-account.json file."
+echo "${GDRIVE_SERVICE_ACCOUNT}" > /tmp/gdrive-service-account.json
+
 echo "Create rclone.conf file."
 cat <<EOF > /tmp/rclone.conf
+[gd]
+type = drive
+scope = drive
+service_account_file = /tmp/gdrive-service-account.json
+client_id = ${GDRIVE_CLIENT_ID}
+root_folder_id = ${GDRIVE_ROOT_FOLDER_ID}
+impersonate = ${GDRIVE_IMPERSONATOR}
+
 [s3]
 type = s3
 env_auth = false
@@ -13,6 +24,7 @@ region = ${AWS_REGION:-"us-east-1"}
 endpoint = ${S3_ENDPOINT}
 acl = ${AWS_ACL}
 storage_class = ${AWS_STORAGE_CLASS}
+
 [gs]
 type = google cloud storage
 project_number = ${GCS_PROJECT_ID}
@@ -21,9 +33,11 @@ object_acl = ${GCS_OBJECT_ACL}
 bucket_acl = ${GCS_BUCKET_ACL}
 location =  ${GCS_LOCATION}
 storage_class = ${GCS_STORAGE_CLASS:-"MULTI_REGIONAL"}
+
 [http]
 type = http
 url = ${HTTP_URL}
+
 [azure]
 type = azureblob
 account = ${AZUREBLOB_ACCOUNT}
