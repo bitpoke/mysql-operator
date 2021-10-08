7b69a849dcb5e0129989761df6970fb75a5393c4
Calin Don <calin@bitpoke.cloud>
2021-10-08 14:57:19 +0300

    Use distroless as base image for mysql-operator and orchestrator images

--- images/mysql-operator-orchestrator/rootfs/usr/local/bin/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-dockerize -no-overwrite -template /usr/local/share/orchestrator/templates/orchestrator.conf.json:/etc/orchestrator/orchestrator.conf.json
-dockerize -no-overwrite -template /usr/local/share/orchestrator/templates/orc-topology.cnf:/etc/orchestrator/orc-topology.cnf
-
-exec "$@"
