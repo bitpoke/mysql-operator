aaeca26350193e961031d54795c908e288ba1eea
guozhi.li <guozhi.li@daocloud.io>
2022-07-04 18:25:05 +0800

    fix comment

--- pkg/apis/mysql/v1alpha1/common_types.go
+++ pkg/apis/mysql/v1alpha1/common_types.go
@@ -2,14 +2,19 @@ package v1alpha1
 
 import "sigs.k8s.io/controller-runtime/pkg/client"
 
+// MysqlResourceDeletionPolicy delete policy defined
 type MysqlResourceDeletionPolicy string
 
 const (
+	// MysqlResourceDeletionPolicyAnnotationKey delete policy key defined
 	MysqlResourceDeletionPolicyAnnotationKey = "mysql-operator.presslabs.org/resourceDeletionPolicy"
-	MysqlResourceDeletionPolicyDelete        = MysqlResourceDeletionPolicy("delete")
-	MysqlResourceDeletionPolicyRetain        = MysqlResourceDeletionPolicy("retain")
+	// MysqlResourceDeletionPolicyDelete delete policy delete
+	MysqlResourceDeletionPolicyDelete = MysqlResourceDeletionPolicy("delete")
+	// MysqlResourceDeletionPolicyRetain delete policy retain
+	MysqlResourceDeletionPolicyRetain = MysqlResourceDeletionPolicy("retain")
 )
 
+// CheckResourceDeletionPolicyRetain parse obj is need to delete
 func CheckResourceDeletionPolicyRetain(obj client.Object) bool {
 	annotations := obj.GetAnnotations()
 	return annotations != nil && MysqlResourceDeletionPolicy(annotations[MysqlResourceDeletionPolicyAnnotationKey]) == MysqlResourceDeletionPolicyRetain
