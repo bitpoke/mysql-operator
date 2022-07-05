739fb96a375d22015950b3402f075ae93d50e4cf
guozhi.li <guozhi.li@daocloud.io>
2022-07-05 10:57:54 +0800

    git squash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 ### Added
+ * `MysqlDatabase` `MysqlUser` Add delete policy
 ### Changed
 ### Removed
 ### Fixed
---
+++ pkg/apis/mysql/v1alpha1/common_types.go
@@ -0,0 +1,21 @@
+package v1alpha1
+
+import "sigs.k8s.io/controller-runtime/pkg/client"
+
+// MysqlResourceDeletionPolicy delete policy defined
+type MysqlResourceDeletionPolicy string
+
+const (
+	// MysqlResourceDeletionPolicyAnnotationKey delete policy key defined
+	MysqlResourceDeletionPolicyAnnotationKey = "mysql-operator.presslabs.org/resourceDeletionPolicy"
+	// MysqlResourceDeletionPolicyDelete delete policy delete
+	MysqlResourceDeletionPolicyDelete = MysqlResourceDeletionPolicy("delete")
+	// MysqlResourceDeletionPolicyRetain delete policy retain
+	MysqlResourceDeletionPolicyRetain = MysqlResourceDeletionPolicy("retain")
+)
+
+// DeletionPolicyRetain parse obj is need to retain
+func DeletionPolicyRetain(obj client.Object) bool {
+	annotations := obj.GetAnnotations()
+	return annotations != nil && MysqlResourceDeletionPolicy(annotations[MysqlResourceDeletionPolicyAnnotationKey]) == MysqlResourceDeletionPolicyRetain
+}
